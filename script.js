c9fd79891dfca9ba89aeb0932bed3023c68fff4d
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-06-28 16:39:22 -0500

    adding base function for apple purchase

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+$('document').ready(function() {
+
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
-var Array = [[{fruit: bananas}, {quantity: 0}, {avgPrice: 0}],
-            [{fruit: apples}, {quantity: 0}, {avgPrice: 0}],
-            [{fruit: oranges}, {quantity: 0}, {avgPrice: 0}],
-            [{fruit: grapes}, {quantity: 0}, {avgPrice: 0}]];
+var inventoryArray = [{bananas: 0}, {apples: 0}, {oranges: 0}, {grapes: 0}];
+
+var commoditiesArray =[{bananas: 2.50}, {apples: 3.25}, {oranges: 5.00}, {grapes: 7.76}]
+
+var totalCash = 100;
+
+var appleTotalSpent = 0;
+
+var appleAvg = 0;
+
+$('#apples').on('click', function() {
+	if (totalCash >= commoditiesArray[1].apples) {
+	inventoryArray[1].apples += 1;
+	appleTotalSpent += commoditiesArray[1].apples;
+	totalCash -= commoditiesArray[1].apples;
+} else {
+	alert('You don\'t have enough money!');
+}
+
+	appleAvg = appleTotalSpent / inventoryArray[1].apples;
+}
+
+
+});
