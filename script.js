647c6e7f2e84c56920abc6711b4569555703a96d
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-06-29 10:39:01 -0500

    created working functions for grapes and oranges

--- script.js
+++ script.js
@@ -10,14 +10,17 @@ var commoditiesArray =[{bananas: 2.50}, {apples: 3.25}, {oranges: 5.00}, {grapes
 
 var totalCash = 100;
 
-
 var appleTotalSpent = 0;
-
 var appleAvg = 0;
 
 var bananaTotalSpent = 0;
-
 var bananaAvg = 0;
+
+var orangeTotalSpent = 0;
+var orangeAvg = 0;
+
+var grapeTotalSpent = 0;
+var grapeAvg = 0;
 
 $('#apples').on('click', function() {
 	if (totalCash >= commoditiesArray[1].apples) {
@@ -55,6 +58,44 @@ $('#bananas').on('click', function() {
 	$('#bananaP').append(bananaAvg);
 	$('#bananasPrice').empty();
 	$('#bananasPrice').append(commoditiesArray[0].bananas);
+});
+
+$('#oranges').on('click', function() {
+	if (totalCash >= commoditiesArray[2].oranges) {
+	inventoryArray[2].oranges += 1;
+	orangeTotalSpent += commoditiesArray[2].oranges;
+	totalCash -= commoditiesArray[2].oranges;
+	$('#wallet').empty();
+	$('#wallet').append(totalCash);
+	$('#orangeQ').empty();
+	$('#orangeQ').append(inventoryArray[2].oranges);
+} else {
+	alert('You don\'t have enough money!');
+}
+	orangeAvg = orangeTotalSpent / inventoryArray[2].oranges;
+	$('#orangeP').empty();
+	$('#orangeP').append(orangeAvg);
+	$('#orangesPrice').empty();
+	$('#orangesPrice').append(commoditiesArray[2].oranges);
+});
+
+$('#grapes').on('click', function() {
+	if (totalCash >= commoditiesArray[3].grapes) {
+	inventoryArray[3].grapes += 1;
+	grapeTotalSpent += commoditiesArray[3].grapes;
+	totalCash -= commoditiesArray[3].grapes;
+	$('#wallet').empty();
+	$('#wallet').append(totalCash);
+	$('#grapeQ').empty();
+	$('#grapeQ').append(inventoryArray[3].grapes);
+} else {
+	alert('You don\'t have enough money!');
+}
+	grapeAvg = grapeTotalSpent / inventoryArray[3].grapes;
+	$('#grapeP').empty();
+	$('#grapeP').append(grapeAvg);
+	$('#grapesPrice').empty();
+	$('#grapesPrice').append(commoditiesArray[3].grapes);
 });
 
 
