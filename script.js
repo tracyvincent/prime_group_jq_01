bc9b66ddc0dcc1c4ff017f5992a305cf91edc4ca
tracyvincent <tracykvincent@gmail.com>
2016-06-29 10:39:06 -0500

    Sets interval to change prices

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $('document').ready(function() {
 
-function randomNumber(min, max) {
-	return Math.floor(Math.random() * (1 + max - min) + min);
-}
+	function randomNumber(min, max) {
+		return Math.floor(Math.random() * (1 + max - min) + min);
+	}
 
 var inventoryArray = [{bananas: 0}, {apples: 0}, {oranges: 0}, {grapes: 0}];
 
-var commoditiesArray =[{bananas: 2.50}, {apples: 3.25}, {oranges: 5.00}, {grapes: 7.76}]
+var commoditiesArray =[{fruit: 'bananas', price: 2.50}, {fruit: 'apples', price: 3.25}, {fruit: 'oranges', price: 5.00}, {fruit: 'grapes', price: 7.76}]
 
 var totalCash = 100;
 
@@ -24,7 +24,19 @@ $('#apples').on('click', function() {
 }
 
 	appleAvg = appleTotalSpent / inventoryArray[1].apples;
+})
+
+function priceChange(fruits){
+	for (i = 0; i < fruits.length; i++){
+	fruits[i].price  += (randomNumber(-50, 50)/100);
+
+		console.log((fruits[i].price).toFixed(2));
+	}
 }
+
+setInterval(function(){ priceChange(commoditiesArray); }, 4000);
+
+
 
 
 });
